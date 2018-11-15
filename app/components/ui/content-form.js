7880695cd842b5c223ddd98034348d0933ac6926
SKallDay <shaistakirmani@gmail.com>
2018-11-15 15:36:03 -0500

    made content form with live preview

---
+++ app/components/ui/content-form.js
@@ -0,0 +1,11 @@
+import Component from '@ember/component';
+import { computed } from "@ember/object";
+
+export default Component.extend({
+    
+    form: computed(function(){
+        return {
+            "content": null
+        }
+    })
+});
